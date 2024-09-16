189cd9989ebcc03962db80400b638d21bb2a093c
Meloundjaj <meloundjaj@nycstudents.net>
2024-09-16 16:27:15 -0400

    lollll
    j

---
+++ Joseph_Meloundja_Lecture2.cpp
@@ -0,0 +1,27 @@
+// Meloundja Joseph 
+// Lecture 2
+// 9/16/2024
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+    //creating varibles 
+    int NumNum = 25;
+    float Floatnum = 89.34;
+    double doublenum = 2.67;
+    char charnum = 'A';
+    bool mybooly = false;
+    string stringy = "i love wingstop";
+    int inches;
+    inches = 100;
+    cout << inches << "  inch(es) = ";
+    cout << inches / 12 << " feet (foot) and ";
+    cout << inches % 12 << " inch(es) " << endl;
+
+    cout << NumNum << " < ";
+    cout << Floatnum << endl;
+
+    return 0;
+}
