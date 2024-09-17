17b55be1a9ce2c0043a7534e2fd1bd2f99d55677
Meloundjaj <meloundjaj@nycstudents.net>
2024-09-17 15:45:04 -0400

    9/17

--- Joseph_Meloundja_Lecture2.cpp
+++ Joseph_Meloundja_Lecture2.cpp
@@ -12,6 +12,8 @@ int main ()
     float Floatnum = 89.34;
     double doublenum = 2.67;
     char charnum = 'A';
+    char charymun = 't';
+    char charynum2 = 'u';
     bool mybooly = false;
     string stringy = "i love wingstop";
     int inches;
@@ -20,8 +22,9 @@ int main ()
     cout << inches / 12 << " feet (foot) and ";
     cout << inches % 12 << " inch(es) " << endl;
 
-    cout << NumNum << " < ";
-    cout << Floatnum << endl;
-
+    cout << " t > u = " << (charymun > charynum2 ) << endl;
+    cout << " 2 + 3.5 = " << 2 + 3.5 << endl;
+    cout << "6 / (4 + 3.9) = " << 6 / (4 + 3.9) << endl; 
+    cout << "5.4 * 2 - (13.6 + 18 /2) = " << 5.4 * 2 - (13.6 + 18 /2) << endl;
     return 0;
 }
